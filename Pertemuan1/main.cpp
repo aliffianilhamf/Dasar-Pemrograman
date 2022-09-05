28bff2604941e8a32bd658ec0f1815395603268e
aliffianilhamf <112859514+aliffianilhamf@users.noreply.github.com>
2022-09-05 21:53:55 +0700

    tugas pertemuan 1 dasar pemrograman

---
+++ Pertemuan1/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Nama : Alifian ilham febriyana" << endl;
+    cout << "Nim : A11.2022.14155" << endl;
+    cout << "Alamat : Desa Dimoro, Dusun Kuwojo, Rt04, Rw05, Kec.Toroh, Kab.Grobogan, Jawa Tengah." << endl;
+    cout << "Asal Sekolah : SMK N 2 Purwodadi" << endl;
+    return 0;
+}
