f94739ccd6fb27f24a42b9e90429ead78f65c68b
aliffianilhamf <112859514+aliffianilhamf@users.noreply.github.com>
2022-09-12 14:45:02 +0700

    Tugas pertemuan 3 dasar pemrograman

---
+++ pertemuan3/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+int main()
+{
+   // cout << "Hello world!" << endl;
+    /*
+    printf("aliffianilham");
+    int
+    float
+    char
+    string*/
+
+    string n = "aliffian" ;
+    int x = 19 ;
+    float t = 165.4 ;
+    char b = 'M' ;
+
+
+    cout <<"Nama saya:"<< n ;
+    cout << endl;
+    cout <<"Umur saya:"<< x ;
+    cout <<"\nTinggi saya:"<< t ;
+    cout <<"\nUkuran baju saya:"<< b ;
+
+
+    return 0;
+}
